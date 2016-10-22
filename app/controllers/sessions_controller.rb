a41341302707cd6897989ecebcc226ce91b54dd4
Jae Byoun <byun7529@gmail.com>
2016-10-22 11:53:56 -0500

    Jae iteration 1 part2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,25 @@
 class SessionsController < ApplicationController
   def new
+    #@user = User.new
   end
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
+    if user #&& user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      redirect_to user
+      render "login_success" # this is inserted for the purpose of a test
+      #redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
-      render 'new'
+      render "login_fail" # this is inserted for the purpose of a test      
     end
+  end
+  
+  def login_success
+  end
+
+  def login_fail
   end
   
   def destroy
