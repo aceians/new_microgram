5ca0f0f91aed87e1b5ced118711a7a54c6532a7b
Savinay Narendra <savinay@tamu.edu>
2016-10-23 03:27:38 +0000

    added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root  'welcome#index'
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  # root 'static_pages#home'
+  root 'welcome#index'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  resources :users
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
