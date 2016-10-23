99ff6457b98743dbbc4c8d899096b8d132e530eb
Jae Byoun <byun7529@gmail.com>
2016-10-23 18:49:14 -0500

    1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,11 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    if user #&& user.authenticate(params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
       # Log the user in and redirect to the user's show page.
       log_in user
-      render "login_success" # this is inserted for the purpose of a test
-      #redirect_to user
+      # render "login_success" # this is inserted for the purpose of a test
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination' # Not quite right!
       render "login_fail" # this is inserted for the purpose of a test      
