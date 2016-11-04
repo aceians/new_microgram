f4db4f43156f154fdc86bc0975134bc4c06949e1
Jae Byoun <byun7529@gmail.com>
2016-11-04 15:07:55 -0500

    basic login complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap
 //= require turbolinks
-//= require bootstrap-sprockets
-//= require jquery.easing
-//= require custom 
 //= require_tree .
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)   
     if @user.save
+      log_in @user
       flash[:success] = "Successfully registered!!"
       redirect_to '/login'
       # Handle a successful save.
