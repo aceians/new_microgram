dd82533491ecb2c06fd530889f5a0cd7ae105ece
Savinay Narendra <savinay@tamu.edu>
2016-11-09 01:57:00 +0000

    merged denny

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108025226) do
+ActiveRecord::Schema.define(version: 20161108221222) do
+
+  create_table "uploads", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "uploads", ["user_id", "created_at"], name: "index_uploads_on_user_id_and_created_at"
+  add_index "uploads", ["user_id"], name: "index_uploads_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
