ffe0e007b68b7063cdf08516d3bdb19dcb6caa61
Jae Byoun <byun7529@gmail.com>
2016-11-09 17:46:05 -0600

    temp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,20 +1,5 @@
 Rails.application.configure do
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.default :charset d=> "utf-8"
-config.action_mailer.default_url_options = { :host => 'infinite-dawn-91717.herokuapp.com' }
-
-config.action_mailer.smtp_settings = {
-  :user_name => ENV["app59097662@heroku.com"],
-  :password => ENV["npdqoxyn1324"],
-  :address => 'smtp.sendgrid.net',
-  :domain => 'myapp.herokuapp.com',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
