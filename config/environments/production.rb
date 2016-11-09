8b26687ed054f3e504d0f0922feab0e1757614df
Jae Byoun <byun7529@gmail.com>
2016-11-09 13:41:00 -0600

    sendgrid by jae

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,14 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'quiet-headland-57860.herokuapp.com'
+  host = 'floating-falls-78957.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['byoun7529'],
-    :password       => ENV['tamuTAMU12'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
