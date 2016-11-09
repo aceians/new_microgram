08290aeff6355209fff50fb8fa296963d8f00732
Nayan Chaudhary <nayanchaudhary@tamu.edu>
2016-11-09 00:45:46 +0000

    submission page for iteration2

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  
   get 'static_pages/home'
 
   get 'static_pages/help'
 
   root 'static_pages#home'
+  
 
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
@@ -12,4 +14,8 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+  
+  resources :uploads
+  
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161104203740) do
+ActiveRecord::Schema.define(version: 20161108232926) do
+
+  create_table "uploads", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image_id"
+    t.string   "text"
+  end
+
+  add_index "uploads", ["user_id"], name: "index_uploads_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
