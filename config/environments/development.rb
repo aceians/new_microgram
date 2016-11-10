c2fff638277a57d59c88319d458c0b127655c47e
Jae Byoun <byun7529@gmail.com>
2016-11-09 22:40:27 -0600

    temp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,13 +6,12 @@ host = 'studyspace-byoun7529.c9users.io'
 config.action_mailer.default_url_options = { host: host}
 ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.gmail.com',
-  :port           => 587,
+  :port           => 465,
   :user_name      => 'byun7529@gmail.com',
   :password       => 'quswornjs2',
   :domain         => 'gmail.com',
   :authentication       => :plain,
-  :enable_starttls_auto => true,
-  :ssl => true
+  :enable_starttls_auto => true
 }
   # Settings specified here will take precedence over those in config/application.rb.
 
