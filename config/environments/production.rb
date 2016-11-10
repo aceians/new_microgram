e30990c656fe99a50a7f75ad908cefbc6911113c
Jae Byoun <byun7529@gmail.com>
2016-11-09 22:57:22 -0600

    temp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :smtp
   host = 'infinite-dawn-91717.herokuapp.com'
@@ -88,8 +88,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['app59097662@heroku.com'],
-    :password       => ENV['npdqoxyn1324'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
