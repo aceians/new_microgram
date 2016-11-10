2d413c0afc223611034237be130e2b424ee35cbd
Jae Byoun <byun7529@gmail.com>
2016-11-10 06:25:38 +0000

    temp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,7 +24,7 @@ ActionMailer::Base.smtp_settings = {
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
