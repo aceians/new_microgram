d8b668b929a173c18eb3de67d3ee5b9bc6b7f4f5
Jae Byoun <byun7529@gmail.com>
2016-11-09 21:26:37 -0600

    temp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,16 @@
 Rails.application.configure do
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'studyspace-byoun7529.c9users.io'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host}
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'smtp.gmail.com',
     :port           => '587',
-    :authentication => :plain,
-    :user_name      => 'byoun7529',
-    :password       => 'tamuTAMU12',
-    :domain         => 'heroku.com',
+    :user_name      => 'byun7529@gmail.com',
+    :password       => 'quswornjs2',
+    :domain         => 'gmail.com',
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
   # Settings specified here will take precedence over those in config/application.rb.
