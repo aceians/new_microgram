c39079f2fa8413ba37bc4dd8fadcedb300477f73
Jae Byoun <byun7529@gmail.com>
2016-11-09 22:11:03 -0600

    temp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = true
+#config.action_mailer.raise_delivery_errors = true
 config.action_mailer.delivery_method = :smtp
 host = 'studyspace-byoun7529.c9users.io'
 config.action_mailer.default_url_options = { host: host}
