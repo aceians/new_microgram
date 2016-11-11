3b281aa013e03eff5f1366bb18ae7eb4678fdea9
Jae Byoun <byun7529@gmail.com>
2016-11-11 05:39:34 +0000

    qwer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,10 +15,10 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  #config.action_mailer.raise_delivery_errors = true
-  #config.action_mailer.delivery_method = :test
-  #host = 'studyspace-byoun7529.c9users.io'
-  #config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'my-project-byoun7529.c9users.io'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
