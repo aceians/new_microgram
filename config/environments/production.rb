a041c076ff79ea7e850ed20bcc7701179dc9d70b
Jae Byoun <byun7529@gmail.com>
2016-11-11 05:57:39 +0000

    temp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
     config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'secret-reef-48218.herokuapp.com'
+  host = 'infinite-dawn-91717.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
