ea0b6e539235efe6958112626a70bbeedfb38d02
Jae Byoun <byun7529@gmail.com>
2016-12-07 04:09:12 +0000

    test

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,6 +3,6 @@ class Image < ApplicationRecord
   has_attached_file :image, styles: { small: "150x150", :medium => "300x300", large: "700x700" }
   #validates :image, :attachment_content_type => { :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]}
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-  validates_attachment_presence :image,   presence: true
-  validates_attachment_size :image, :less_than => 10.megabytes
+  validates_attachment_presence :image
+  validates_attachment_size :image, :less_than => 5.megabytes
 end
